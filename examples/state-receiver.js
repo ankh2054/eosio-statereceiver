d68f457383231367c698690b9797e1c7b224fc9d
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2023-07-24 20:01:45 +0100

    Update state-receiver.js

--- examples/state-receiver.js
+++ examples/state-receiver.js
@@ -1,5 +1,6 @@
 const StateReceiver = require('../src/state-receiver');
 
+
 const sr = new StateReceiver({
   logger: {
     info: (...m) => console.info(...m),
@@ -7,70 +8,28 @@ const sr = new StateReceiver({
     warn: (...m) => console.warn(...m),
     error: (...m) => console.error(...m),
   },
-  startBlock: 694,
-  socketAddresses: [process.env.SOCKET_ADDRESS || 'ws://localhost:8080'],
-  eosEndpoint: process.env.EOS_ENDPOINT || 'http://localhost:8888',
+  startBlock: 228671233,
+  irreversible: true,
+  socketAddresses: [process.env.SOCKET_ADDRESS || 'ws://88.198.18.252:29876'],
+  eosEndpoint: process.env.EOS_ENDPOINT || 'http://88.198.18.252:28888',
   deserializerActions: [
-    'eosio.token::transfer',
-    'bridge.wax::reqnft',
-    'bridge.wax::reqwaxtoeth',
-    'bridge.wax::nft2wax',
-    'returnvalue::returnint',
-    'returnvalue::returnstruct',
-    'returnvalue::returnstring',
+
   ],
   maxQueueSize: 10,
 });
-
-let blockCount = 0;
-let blockCountT0 = blockCount;
-const statSecond = 2;
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const statInterval = setInterval(() => {
-  const num_messages = blockCount - blockCountT0;
-  const status = sr.status();
-  console.log(
-    `Received ${num_messages / statSecond} B/s; Queue size: ${status.serializedMessageQueueSize}`
-  );
-  blockCountT0 = blockCount;
-}, statSecond * 1000);
-
 sr.registerTraceHandler({
-  async processTrace(block_num, traces) {
+  async processTrace(block_num, traces, block) {
     await sleep(1000);
 
-    blockCount++;
-
-    traces.forEach(([_traceVersion, traceData]) => {
-      const action_traces = traceData.action_traces;
-      action_traces.forEach(([_actionTraceVersion, actionTraceData]) => {
-        const act = actionTraceData.act;
-        const contractName = act.account;
-        const actionName = act.name;
-
-        if (contractName === 'eosio.token' && actionName === 'transfer') {
-          console.log(
-            `${block_num} ${contractName}::${actionName} ${act.data.from} -> ${act.data.to} ${act.data.quantity}`
-          );
-        } else if (contractName === 'bridge.wax' && actionName === 'reqwaxtoeth') {
-          console.log(`${block_num} ${contractName}::${actionName} ${act.data}`);
-        } else if (contractName === 'bridge.wax' && actionName === 'reqnft') {
-          console.log(`${block_num} ${contractName}::${actionName} ${act.data}`);
-        } else if (contractName === 'bridge.wax' && actionName === 'nft2wax') {
-          console.log(`${block_num} ${contractName}::${actionName} ${act.data.to_account}`);
-        } else if (contractName === 'returnvalue') {
-          console.log(
-            `${block_num} ${contractName}::${actionName} return value ${JSON.stringify(
-              actionTraceData.return_value
-            )}`
-          );
-        }
-      });
-    });
+    //blockCount++;
+    console.log(block_num)
+    console.log(block)
+    
   },
 });
 
